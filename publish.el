737b50b74bc8dfb83b7a64c851657a1461d31ab1
Shane Panter <shane@foundationcode.com>
2024-03-10 16:08:22 -0600

    Working on new org site

--- publish.el
+++ publish.el
@@ -43,6 +43,14 @@ representation for the files to include, as returned by
 			   :publishing-directory ,(concat (vc-root-dir) "build/")
 			   :publishing-function org-html-publish-to-html)
 
+			  ("cs408"
+			   :base-directory ,(concat (vc-root-dir)  "org/cs408")
+			   :base-extension "org"
+			   :recursive t
+			   :publishing-directory ,(concat (vc-root-dir) "build/cs408")
+			   :publishing-function org-html-publish-to-html
+			   :html-postamble "<ul><li>Author: %a (%e)</li><li>Date: %d</li><li>Exported: %T</li><li>%v</li></ul>")
+
 			  ("teaching"
 			   :base-directory ,(concat (vc-root-dir)  "org/teaching")
 			   :base-extension "org"
@@ -50,29 +58,37 @@ representation for the files to include, as returned by
 			   :auto-sitemap t
 			   :sitemap-title ""
 			   :sitemap-filename "sitemap.org"
+			   :sitemap-sort-files chronologically
 			   :sitemap-format-entry my-org-publish-sitemap-entry
 			   :sitemap-function my-org-publish-sitemap
 			   :publishing-directory ,(concat (vc-root-dir) "build/teaching")
 			   :publishing-function org-html-publish-to-html
-			   :html-postamble "<ul><li>Author: %a (%e)</li><li>Exported: %T</li></ul>")
+			   :html-postamble "<ul><li>Author: %a (%e)</li><li>Date: %d</li><li>Exported: %T</li><li>%v</li></ul>")
 
-			  ("blog"
-			   :base-directory ,(concat (vc-root-dir)  "org/blog")
+			  ("teaching-images"
+			   :base-directory ,(concat (vc-root-dir)  "org/teaching/images")
+			   :base-extension "jpg\\|gif\\|png"
+			   :publishing-directory ,(concat (vc-root-dir) "build/teaching/images")
+			   :publishing-function org-publish-attachment)
+
+			  ("research"
+			   :base-directory ,(concat (vc-root-dir)  "org/research")
 			   :base-extension "org"
 			   :recursive t
 			   :auto-sitemap t
 			   :sitemap-title ""
 			   :sitemap-filename "sitemap.org"
+			   :sitemap-sort-files chronologically
 			   :sitemap-format-entry my-org-publish-sitemap-entry
 			   :sitemap-function my-org-publish-sitemap
-			   :publishing-directory ,(concat (vc-root-dir) "build/blog")
+			   :publishing-directory ,(concat (vc-root-dir) "build/research")
 			   :publishing-function org-html-publish-to-html
-			   :html-postamble "<ul><li>Author: %a (%e)</li><li>Exported: %T</li></ul>")
+			   :html-postamble "<ul><li>Author: %a (%e)</li><li>Date: %d</li><li>Exported: %T</li><li>%v</li></ul>")
 
 			  ("images"
-			   :base-directory ,(concat (vc-root-dir)  "org/img")
+			   :base-directory ,(concat (vc-root-dir)  "org/images")
 			   :base-extension "jpg\\|gif\\|png"
-			   :publishing-directory ,(concat (vc-root-dir) "build/img")
+			   :publishing-directory ,(concat (vc-root-dir) "build/images")
 			   :publishing-function org-publish-attachment)
 
 			  ("css"
@@ -81,18 +97,13 @@ representation for the files to include, as returned by
 			   :publishing-directory ,(concat (vc-root-dir) "build/css")
 			   :publishing-function org-publish-attachment)
 			  ("website"
-			   :components ("index" "teaching" "images" "css"))))
+			   :components ("index" "teaching" "research" "images" "css"))))
 
 
 (defun skp-publish ()
   (interactive)
-  (let ((default-directory (vc-root-dir)))
-    (message default-directory)
-    ;(org-publish-remove-all-timestamps)
-    ;(org-publish-reset-cache)
-    (org-publish-all)))
+  (org-publish-all))
 
-;
 
 ;;If running in batch mode
 (if noninteractive
